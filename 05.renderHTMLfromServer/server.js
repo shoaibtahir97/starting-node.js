c8cd60fbe3aa66e92cf6ea10586dc4c355a72de0
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-07-29 21:00:32 +0500

    render html file from server

---
+++ 05.renderHTMLfromServer/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = 6011;
+
+const app = express();
+app.get("/html", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
